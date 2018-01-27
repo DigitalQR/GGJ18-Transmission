8e549a24184861d54fc477f0c3b155c123b60499
DigitalQR <sam_pot_97@hotmail.com>
2018-01-27 10:59:37 +0000

    Added laser aiming

---
+++ Assets/Management/Props/LaserHandles.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserHandles : InteractableBehaviour
+{
+	[SerializeField]
+	private Transform laserBase;
+	[SerializeField]
+	private Transform laserBody;
+	private PlayerCharacter currentGrabbed;
+
+	private Quaternion bodyStartRotation;
+
+
+	void Start()
+	{
+		bodyStartRotation = laserBody.rotation;
+	}
+
+	void Update ()
+	{
+		if (currentGrabbed != null)
+		{
+			Vector3 direction = laserBase.position - currentGrabbed.transform.position;
+			direction.y = 0;
+			laserBody.forward = direction;
+			laserBody.rotation = laserBody.rotation * bodyStartRotation;
+		}
+	}
+
+	public override void Interact(PlayerCharacter character)
+	{
+		currentGrabbed = character;
+	}
+
+	void OnTriggerExit(Collider coll)
+	{
+		if (currentGrabbed != null && coll.gameObject == currentGrabbed.gameObject)
+			currentGrabbed = null;
+	}
+}
