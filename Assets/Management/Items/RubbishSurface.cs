779f6406241615cd82063f575c10d5bec685d021
DigitalQR <sam_pot_97@hotmail.com>
2018-01-27 06:59:37 +0000

    Added rubbish bin

---
+++ Assets/Management/Items/RubbishSurface.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubbishSurface : InteractableBehaviour
+{
+	public override void Interact(PlayerCharacter character)
+	{
+		if (character.heldItem != null)
+		{
+			Destroy(character.heldItem.gameObject);
+			character.heldItem = null;
+		}
+	}
+}
