028fac2cdf5bed89cbf8a2e7492395d64e0bc080
DigitalQR <sam_pot_97@hotmail.com>
2018-01-27 08:32:56 +0000

    Added item spawning buttons

---
+++ Assets/Management/Items/ItemSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : InteractableBehaviour
+{
+	public Item targetItem;
+	public ItemSurface targetSurface;
+
+
+	public override void Interact(PlayerCharacter character)
+	{
+		Debug.Log("A");
+		if (!targetSurface.holdingItem)
+		{
+			Debug.Log("B");
+			Item item = Instantiate(targetItem.gameObject).GetComponent<Item>();
+			if (!targetSurface.AttemptPlace(item))
+			{
+				Debug.Log("C");
+				Destroy(item.gameObject);
+			}
+			else
+
+				Debug.Log("D");
+		}
+	}
+}
